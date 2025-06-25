83cd93b3d361cc3cb9c5317950b34b5870a054a4
FahimX <sakibfahim270@gmail.com>
2025-06-25 19:22:01 +0800

    auth

---
+++ src/Layouts/AuthLayout.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Outlet } from "react-router";
+import authImage from "../assets/authImage.png";
+import ProFastLogo from "../Pages/shared/proFastLogo/proFastLogo";
+
+const AuthLayout = () => {
+  return (
+    <div>
+      <div className="p-12 bg-base-200 ">
+        <div>
+          <ProFastLogo></ProFastLogo>
+        </div>
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="flex-1 bg-[#FAFDF0]">
+            <img src={authImage} className="max-w-sm rounded-lg shadow-2xl" />
+          </div>
+          <div className="flex-1 ">
+            <Outlet></Outlet>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthLayout;
